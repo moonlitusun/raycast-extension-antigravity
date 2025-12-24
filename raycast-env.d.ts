c84149c9b4150bcf4d677829d66b02344fc816d0
Roc <moonlitusun@gmail.com>
2025-12-24 17:46:45 +0800

    feat: init project

---
+++ raycast-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Projects Directory - The path to the directory to search for projects */
+  "projectsDirectory": string,
+  /** Search Depth - How many levels deep to search for folders (1-5) */
+  "searchDepth": string,
+  /** Antigravity Command Path - Full path to the agy command (leave empty to use default: ~/.antigravity/antigravity/bin/agy) */
+  "agyCommandPath": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `index` command */
+  export type Index = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `index` command */
+  export type Index = {}
+}
+
